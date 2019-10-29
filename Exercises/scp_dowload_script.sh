0e17ec5ecf909a63c7e77036f1c6a57811f3c2d6
tabatom <tommytab@hotmail.it>
2019-10-29 14:39:05 +0100

    Deleting a file

--- Exercises/scp_dowload_script.sh
+++
@@ -1,6 +0,0 @@
-scp tabarelt@spiro.fisica.unipd.it:/home/tabarelt/Quantum_Information/$1/$2/* $2
-# Deve essere gestito meglio: voglio rinominare appositamente tutti gli eseguibili perché il compilatore fortran sulla spiro è vecchio rispetto a quello locale
-# mv $1/$2/Ex*.x $1/$2/Ex*_old.x
-
-# Some hints to use grep to get all executable files (named *.x)
-# ls -lR | grep "\.x"
